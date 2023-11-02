cd59c1cd548c57990808f7efa9142e161acd7184
yuris <yls4@cornell.edu>
2023-11-02 15:02:58 -0400

    finished routes

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: {format: :json} do
+    get '/pokemon/types', to: 'pokemon#types', as: 'types'
+    resources :pokemon, only: [:index, :show, :create, :update] do 
+      resources :items, only: [:index, :create]
+    end
+    resources :items, only: [:update, :destroy]
+  end
 end
