41d5c945074d8631b0781aa263bc5e77e9a23b91
yuris <yls4@cornell.edu>
2023-11-02 14:01:20 -0400

    annotated models

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
 end
--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/poke_move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/poke_move.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: poke_moves
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  move_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PokeMove < ApplicationRecord
 end
